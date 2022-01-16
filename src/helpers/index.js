b54c76b51d83221931e045bb97bef4894f2f4a64
onihani <n.bongo40@gmail.com>
2022-01-16 19:46:55 +0000

    fixed Date.parse not working on iphone/aple

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,5 +1,5 @@
 export function getTimeRemaining(endtime) {
-  const total = Date.parse(endtime) - Date.parse(new Date());
+  const total = new Date(endtime).getTime() - Date.now();
   const seconds = Math.floor((total / 1000) % 60);
   const minutes = Math.floor((total / 1000 / 60) % 60);
   const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
