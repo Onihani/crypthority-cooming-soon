2dacc2053647eb0f0e9e68a8d2b583d6a1e9d55c
onihani <n.bongo40@gmail.com>
2022-01-16 19:59:23 +0000

    changed date month to january

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import { FaInstagram, FaTwitter, FaYoutube, FaTiktok } from "react-icons/fa";
 import { getTimeRemaining } from "../src/helpers";
 
 function Home() {
-  const launchDate = new Date(2022, 1, 25);
+  const launchDate = new Date(2022, 0, 25);
   const initialDate = getTimeRemaining(launchDate);
 
   const daysRef = useRef();
